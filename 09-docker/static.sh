f21f4a8d14f497c3e8b8bb9b3a002bb226b792a1
Maxim Kolchin <kolchin.mi@gmail.com>
2023-01-23 23:37:54 +0300

    small changes

--- 09-docker/static.sh
+++ 09-docker/static.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#docker run -ti --name static-site -v index.html:/var/www/html/ -p 8081:80 hometask-image
 docker run -d --name static-site -p 8081:80 hometask-image
 docker exec -d static-site rm /var/www/html/index.php
 docker cp index.html static-site:/var/www/html
